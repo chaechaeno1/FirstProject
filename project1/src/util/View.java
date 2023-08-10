0235bb07d4023dfba8a7be8a71f6ab68a1a678e6
CHAE <chaechaeno1@gmail.com>
2023-08-10 21:35:19 +0900

    project0810

---
+++ project1/src/util/View.java
@@ -0,0 +1,31 @@
+package util;
+
+public interface View {
+   int HOME = 1;
+   int MAIN = 11;
+
+   int CUSTOMER = 2;
+   int CUSTOMER_SIGNUP = 21;
+   int CUSTOMER_LOGIN = 22;
+   int CUSTOMER_RESIGN = 23;
+   
+   int TICKET = 3;
+   int TICKET_SEARCHING = 31;
+   int TICKET_SEARCHING_STATION = 32;
+   int TICKET_SEARCHING_DATE = 33;
+   int TICKET_SEARCHING_BUS_SEAT = 34;
+   int TICKET_BUY = 35;
+   
+   int REFUND = 4;
+   int REFUND_SEARCHING_TICKET = 41;
+   int REFUND_TICKET = 42;
+
+
+   int MANAGE = 5;
+   int MANAGE_LOGIN = 51;
+   int MANAGE_SERVICE = 52;
+   int MANAGE_STATION = 53;
+   int MANAGE_SALES = 54;
+   
+   
+}
